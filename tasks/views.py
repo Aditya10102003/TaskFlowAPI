9d62a9550a811366a0d57cd4c93dfc509d94009a
adityayadav10102003@gmail.com <adityayadav10102003@gmail.com>
2026-07-12 03:20:55 +0530

    Complete Task CRUD operations and filtering

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        queryset = Task.objects.filter(user=self.request.user)
+
+        status = self.request.query_params.get("status")
+        priority = self.request.query_params.get("priority")
+
+        if status:
+            queryset = queryset.filter(status=status)
+
+        if priority:
+            queryset = queryset.filter(priority=priority)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TaskRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
